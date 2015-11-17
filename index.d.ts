9bfb8ebb1cf82abe2e16b00d6e0f59492224728e
Blake Embrey <hello@blakeembrey.com>
2015-11-17 12:51:26 -0800

    Update definition to use type intersection

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,37 @@
-// Type definitions for minimist 1.1.3
-// Project: https://github.com/substack/minimist
-// Definitions by: Bart van der Schoor <https://github.com/Bartvds>, Necroskillz <https://github.com/Necroskillz>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
-declare function minimist (args?: string[], opts?: minimist.Options): minimist.ParsedArgs;
+declare function minimist <T> (args?: string[], opts?: minimist.Options): minimist.ParsedArgs & T;
 
 declare module minimist {
-	export interface Options {
-		// a string or array of strings argument names to always treat as strings
-		// string?: string;
-		string?: string|string[];
-		// a string or array of strings to always treat as booleans
-		// boolean?: string;
-		boolean?: boolean|string|string[];
-		// an object mapping string names to strings or arrays of string argument names to use
-		// alias?: {[key:string]: string};
-		alias?: {[key:string]: string[]};
-		// an object mapping string argument names to default values
-		default?: {[key:string]: any};
-		// when true, populate argv._ with everything after the first non-option
-		stopEarly?: boolean;
-		// a function which is invoked with a command line parameter not defined in the opts configuration object.
-		// If the function returns false, the unknown option is not added to argv
-		unknown?: (arg: string) => boolean;
-		// when true, populate argv._ with everything before the -- and argv['--'] with everything after the --
-		'--'?: boolean;
-	}
+  export interface Options {
+    // A string or array of strings argument names to always treat as strings.
+    string?: string | string[];
+    // A boolean, string or array of strings to always treat as booleans. If
+    // true will treat all double hyphenated arguments without equal signs as
+    // boolean (e.g. affects --foo, not -f or --foo=bar).
+    boolean?: boolean | string | string[];
+    // An object mapping string names to strings or arrays of string argument
+    // names to use as aliases.
+    alias?: {
+      [key:string]: string[];
+    };
+    // An object mapping string argument names to default values.
+    default?: {
+      [key:string]: any;
+    };
+    // When true, populate argv._ with everything after the first non-option
+    stopEarly?: boolean;
+    // When true, populate argv._ with everything before the -- and argv['--']
+    // with everything after the --.
+    '--'?: boolean;
+    // A function which is invoked with a command line parameter not defined in
+    // the opts configuration object. If the function returns false, the
+    // unknown option is not added to argv.
+    unknown?: (arg: string) => boolean;
+  }
 
-	export interface ParsedArgs {
-		[arg: string]: any;
-		_: string[];
-	}
+  export interface ParsedArgs {
+    _: string[];
+    [arg: string]: any;
+  }
 }
 
 export = minimist;
